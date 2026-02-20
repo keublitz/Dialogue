59acd1a19e4b0f93fe114caeb1704449e657b420
keublitz <samuelwiener@gmail.com>
2026-02-19 19:18:10 -0500

    Add opacity decoding into init?(hex:) for Color

--- Sources/Dialogue/DialogueColor.swift
+++ Sources/Dialogue/DialogueColor.swift
@@ -41,11 +41,21 @@ public extension Color {
         var rgb: UInt64 = 0
         guard Scanner(string: formattedHex).scanHexInt64(&rgb) else { return nil }
         
-        let r = Double((rgb >> 16) & 0xFF) / 255.0
-        let g = Double((rgb >> 8) & 0xFF) / 255.0
-        let b = Double(rgb & 0xFF) / 255.0
-        
-        self.init(red: r, green: g, blue: b)
+        if formattedHex.count == 8 {
+            let r = CGFloat((rgb >> 24) & 0xFF) / 255.0
+            let g = CGFloat((rgb >> 16) & 0xFF) / 255.0
+            let b = CGFloat((rgb >> 8) & 0xFF) / 255.0
+            let a = CGFloat(rgb & 0xFF) / 255.0
+            
+            self.init(red: r, green: g, blue: b, opacity: a)
+        }
+        else {
+            let r = CGFloat((rgb >> 16) & 0xFF) / 255.0
+            let g = CGFloat((rgb >> 8) & 0xFF) / 255.0
+            let b = CGFloat(rgb & 0xFF) / 255.0
+            
+            self.init(red: r, green: g, blue: b)
+        }
     }
 }
 
